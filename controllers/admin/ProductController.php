191c8858dba18cc0272d19a72b7a5b95cc6c5cad
thuyvt <thuyvt1005@gmail.com>
2025-11-17 13:55:51 +0700

    day 4

--- controllers/admin/ProductController.php
+++ controllers/admin/ProductController.php
@@ -10,7 +10,8 @@ class ProductController
     public function index () {
         $view = 'product/index';
         $title = 'Danh sách sản phẩm';
-        // $data = $this->productModel->getAll();
+        $data = $this->productModel->getAll();
+        require_once PATH_VIEW_ADMIN_MAIN;
     }
 }
 ?>
--- models/Product.php
+++ models/Product.php
@@ -3,7 +3,7 @@
         protected $table = 'products';
 
         public function getAll() {
-            $sql = "SELECT * FROM products ORDER BY id DESC;";
+            $sql = "SELECT * FROM products ORDER BY id DESC";
             $stmt = $this->pdo->prepare($sql);
             $stmt->execute();
             return $stmt->fetchAll();
